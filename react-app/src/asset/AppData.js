791a74749f08f8f83142354f9f17023a710af4aa
Abhay Balip <121150307+abhaybalip@users.noreply.github.com>
2025-04-12 21:37:42 +0530

    code change

--- react-app/src/asset/AppData.js
+++ react-app/src/asset/AppData.js
@@ -1,54 +1,67 @@
-
-import Home from '../component/Home'
-import Team from '../component/Team'
-import Predict from '../component/Predict'
+import Home from '../component/Home';
+import Team from '../component/Team';
+import Predict from '../component/Predict';
 
 import { FaGithub, FaLinkedin } from 'react-icons/fa6';
 import { CiMail } from "react-icons/ci";
 
 const NavList = [
     {
-        name: 'Home', path: '/', component: Home
-    }, {
-        name: 'Predictor', path: '/predict', component: Predict
-    }, {
-        name: 'Team', path: '/team', component: Team
+        name: 'Home',
+        path: '/',
+        component: Home
+    },
+    {
+        name: 'Predictor',
+        path: '/predict',
+        component: Predict
+    },
+    {
+        name: 'Team',
+        path: '/team',
+        component: Team
     },
 ]
 
-const TeamData = [
+const defaultLinks = [
+    { name: 'email', logo: CiMail },
+    { name: 'github', logo: FaGithub },
+    { name: 'linkedin', logo: FaLinkedin },
+]
 
+const TeamData = [
     {
         name: 'Abhay Balip',
         links: [
-            { name: 'email', link: 'mailto:abhay.balip01@gmail.com', logo: CiMail },
-            { name: 'github', link: 'https://github.com/abhaybalip/', logo: FaGithub },
-            { name: 'linkedin', link: 'https://linkedin.com/in/abhaybalip/', logo: FaLinkedin },
-        ],
-    },
-    {
-        name: 'Hamza Patel',
-        links: [
-            { name: 'email', link: 'mailto:abhay.balip01@gmail.com', logo: CiMail },
-            { name: 'github', link: 'https://github.com/abhaybalip/', logo: FaGithub },
-            { name: 'linkedin', link: 'https://linkedin.com/in/abhaybalip/', logo: FaLinkedin },
-        ],
-    },
-    {
-        name: 'Raj Malanadkar',
-        links: [
-            { name: 'email', link: 'mailto:abhay.balip01@gmail.com', logo: CiMail },
-            { name: 'github', link: 'https://github.com/abhaybalip/', logo: FaGithub },
-            { name: 'linkedin', link: 'https://linkedin.com/in/abhaybalip/', logo: FaLinkedin },
+            { ...defaultLinks[0], link: 'mailto:abhay.balip@gmail.com' },
+            { ...defaultLinks[1], link: 'https://github.com/abhaybalip/' },
+            { ...defaultLinks[2], link: 'https://linkedin.com/in/abhaybalip/' },
         ],
     },
     {
         name: 'Shyam Kamble',
         links: [
-            { name: 'email', link: 'mailto:abhay.balip01@gmail.com', logo: CiMail },
-            { name: 'github', link: 'https://github.com/abhaybalip/', logo: FaGithub },
-            { name: 'linkedin', link: 'https://linkedin.com/in/abhaybalip/', logo: FaLinkedin },
+            { ...defaultLinks[0], link: 'mailto:shyam.kamble@gmail.com' },
+            { ...defaultLinks[1], link: 'https://github.com/shyamkamble/' },
+            { ...defaultLinks[2], link: 'https://linkedin.com/in/shyamkamble/' },
+        ],
+    },
+    {
+        name: 'Raj Malanadkar',
+        links: [
+            { ...defaultLinks[0], link: 'mailto:raj.malanadkar@gmail.com' },
+            { ...defaultLinks[1], link: 'https://github.com/rajmalanadkar/' },
+            { ...defaultLinks[2], link: 'https://linkedin.com/in/rajmalanadkar/' },
+        ],
+    },
+    {
+        name: 'Hamza Patel',
+        links: [
+            { ...defaultLinks[0], link: 'mailto:hamza.patel@gmail.com' },
+            { ...defaultLinks[1], link: 'https://github.com/hamzapatel/' },
+            { ...defaultLinks[2], link: 'https://linkedin.com/in/hamzapatel/' },
         ],
     },
 ]
-export { NavList, TeamData };
+
+export { NavList, TeamData }
